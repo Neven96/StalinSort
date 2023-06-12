624096236bf491e0e04f0dcf7f6edf1753b83f00
Even <even.holmen@hotmail.no>
2023-06-12 15:42:41 +0200

    Added a random array creator

--- python/stalinsort.py
+++ python/stalinsort.py
@@ -1,10 +1,28 @@
 # -*- coding: utf-8 -*-
+import random
 
-def stalin_sort(unsorted_array):
+def create_array(array_length, array_range):
+    '''
+    Creates an unsorted array of random length and number range
+    Args:
+        array_length (int): how long the unsorted array will be
+        array_range (int): how wide the range of the numbers in the array will be
+    Returns:
+        list: an unsorted array of integers
+    '''
+
+    unsorted_array = []
+
+    for i in range(1, array_length+1):
+        unsorted_array.append(random.randint(1, array_range))
+
+    return unsorted_array
+
+def stalinSort(unsorted_array):
     '''
     Sorts an array, Stalin method
     Args:
-        unsorted_array (list): an array for sorting
+        unsorted_array (list): the unsorted array
     Returns:
         list: the properly sorted array
     '''
@@ -22,3 +40,14 @@ def stalin_sort(unsorted_array):
             j += 1
 
     return sorted_array
+
+
+def main():
+    unsorted_array = create_array(100, 1000)
+    sorted_array = stalinSort(unsorted_array)
+
+    print("Unsorted: ",unsorted_array)
+    print("Sorted: ",sorted_array)
+
+if __name__ == '__main__':
+    main()
